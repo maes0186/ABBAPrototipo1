2a1efc53419351192f374ace9e01555ab6515f74
maes0186 <maes0186@aa3da725-fa38-4a37-849c-0e1cb1b97359>
2012-08-08 04:52:09 +0000

    Ejemplo  Menu

---
+++ sueDreams/sueJar/src/main/java/org/Beans.java
@@ -0,0 +1,14 @@
+package org;
+
+public class Beans {
+	private String nombre;
+
+	public String getNombre() {
+		return nombre+"Prueba";
+	}
+
+	public void setNombre(String nombre) {
+		this.nombre = nombre;
+	}
+
+}
