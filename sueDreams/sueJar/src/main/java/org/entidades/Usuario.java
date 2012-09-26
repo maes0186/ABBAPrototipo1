22ae8e8a87545cc8d217951f63ecf863afdc3ae4
maes0186 <maes0186@aa3da725-fa38-4a37-849c-0e1cb1b97359>
2012-09-26 02:17:37 +0000

    Implementacion J2EE

---
+++ sueDreams/sueJar/src/main/java/org/entidades/Usuario.java
@@ -0,0 +1,54 @@
+package org.entidades;
+
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.Id;
+import javax.persistence.Table;
+
+
+@Entity
+@Table(name="USUARIO")
+public class Usuario {
+	private int id;
+	private String apellido1;
+	private String apellido2;
+	private String nombre1;
+	private String nombre2;
+	@Id
+    @Column(name="ID")
+	public int getId() {
+		return id;
+	}
+	public void setId(int id) {
+		this.id = id;
+	}
+	@Column(name="APELLIDO1")
+	public String getApellido1() {
+		return apellido1;
+	}
+	public void setApellido1(String apellido1) {
+		this.apellido1 = apellido1;
+	}
+	@Column(name="APELLIDO2")
+	public String getApellido2() {
+		return apellido2;
+	}
+	public void setApellido2(String apellido2) {
+		this.apellido2 = apellido2;
+	}
+	@Column(name="NOMBRE1")
+	public String getNombre1() {
+		return nombre1;
+	}
+	public void setNombre1(String nombre1) {
+		this.nombre1 = nombre1;
+	}
+	@Column(name="NOMBRE2")
+	public String getNombre2() {
+		return nombre2;
+	}
+	public void setNombre2(String nombre2) {
+		this.nombre2 = nombre2;
+	}
+
+}
